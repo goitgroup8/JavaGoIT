34052a2197cce5d90e481e1843da2272bb324eae
artkoval4 <artkoval4@gmail.com>
2016-11-05 17:39:39 +0200

    0.2

---
+++ src/bookingsystem/model/User.java
@@ -0,0 +1,117 @@
+package bookingsystem.model;
+
+import java.util.Date;
+
+public class User implements BaseEntity{
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String city;
+
+    private boolean isActive;
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
+    public boolean isLoggedIn() {
+        return isLoggedIn;
+    }
+
+    public void setLoggedIn(boolean loggedIn) {
+        isLoggedIn = loggedIn;
+    }
+
+    public Date getDateRegistered() {
+        return dateRegistered;
+    }
+
+    public void setDateRegistered(Date dateRegistered) {
+        this.dateRegistered = dateRegistered;
+    }
+
+    public Date getDateLastActive() {
+        return dateLastActive;
+    }
+
+    public void setDateLastActive(Date dateLastActive) {
+        this.dateLastActive = dateLastActive;
+    }
+
+    private boolean isLoggedIn;
+
+    private Date dateRegistered;
+    private Date dateLastActive;
+
+
+    public User(long id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.city = city;
+
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", city='" + city + '\'' +
+                '}';
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+    @Override
+    public long getId() {
+
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+
+        User user = (User) o;
+
+        return getId() == user.getId();
+
+    }
+
+    @Override
+    public int hashCode() {
+        return getCity().hashCode();
+    }
+}
